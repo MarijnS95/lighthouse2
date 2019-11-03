2e8cf8da15437a70b789fdd2b7a383929a3bffc9
Marijn Suijten <m.suijten@students.uu.nl>
2019-11-04 00:24:45 +0100

    CUDA: Add PBRT Glass material.

---
+++ lib/RenderSystem/common_materials.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace common
+{
+namespace materials
+{
+namespace pbrt
+{
+#include "./materials/pbrt/core_materials.h"
+}; // namespace pbrt
+}; // namespace materials
+}; // namespace common
