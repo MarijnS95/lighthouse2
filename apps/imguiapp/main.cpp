f1f932b9c667a6e706b1dff583c07b5ff8efbce8
Marijn Suijten <m.suijten@students.uu.nl>
2019-11-11 01:33:05 +0100

    Add PBRT lamp model.

--- apps/imguiapp/main.cpp
+++ apps/imguiapp/main.cpp
@@ -127,8 +127,9 @@ void PrepareScene()
 	pbrtInit( opts, renderer->GetScene() );
 
 	// pbrtParseFile( "data/coffee/scene.pbrt" );
-	pbrtParseFile( "data/material-testball/scene.pbrt" );
+	// pbrtParseFile( "data/material-testball/scene.pbrt" );
 	// pbrtParseFile( "data/glass-of-water/scene.pbrt" );
+	pbrtParseFile( "data/lamp/scene.pbrt" );
 
 	// TODO in end of main!
 	// pbrtCleanup();
